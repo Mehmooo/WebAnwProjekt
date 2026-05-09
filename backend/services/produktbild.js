e30930716f4500d5e8b25651ec686707788bbc41
Mehmoo <138767837+Mehmooo@users.noreply.github.com>
2026-05-09 19:11:07 +0200

    Erste Get Route loadPicture angelegt

--- backend/services/produktbild.js
+++ backend/services/produktbild.js
@@ -5,4 +5,22 @@ var serviceRouter = express.Router();
 
 console.log(' Service Produktbild ');
 
+serviceRouter.get('loadPicture/:id', function(request, response) {
+    const produktbildDao = new ProduktbildDao(request.app.locals.dbConnection);
+
+    try {
+        var obj = produktbildDao.loadById(request.body.id);
+        response.status(200).json(obj);
+    } catch (ex) {
+        console.log('Service Produktbild: Fehler beim Laden der Ressource. Fehler Nachricht: ' + ex.message);
+        response.status(400).json({
+            'fehler': true,
+            'nachricht': ex.message
+        });
+    }
+})
+
+
+
+
 module.exports = serviceRouter;
