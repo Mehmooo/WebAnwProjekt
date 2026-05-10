5024a609dc5afb2ef0d7c1f4510b983eb9e497e3
Mehmoo <138767837+Mehmooo@users.noreply.github.com>
2026-05-11 00:37:10 +0200

    Produktübersichtsseite dynamisch laden erweitert, ProduktId über URL weitergeben

--- frontend/scripts/fetchRequests.js
+++ frontend/scripts/fetchRequests.js
@@ -41,24 +41,41 @@ async function loadAllProductsInOverview(endpoint) {
         if (response.status === 200) {
             console.log('Status Successful, now the new API Call for loadPicture')
             var divParent = document.querySelector('.products_overview_grid');
-            var i = 0;
-            for (let box of divParent.children) {
+            for (let i = 0; i < data.length; i++) {
                 console.log("In der for loop");
-                console.log(data[i].preis);
-                console.log(data[i].bildpfad);
+                let a = document.createElement('a');
+                let div = document.createElement('div');
                 let img = document.createElement('img');
                 let p = document.createElement('p');
-                let div = document.createElement('div');
-                p.innerHTML = `${data[i].bezeichnung}<br>${data[i].preis}`
+                a.classList.add('product-card');
+                a.href = `product_detail.html?id=${data[i].id}`;
+                div.classList.add('products_square');
                 img.src = data[i].bildpfad;
                 img.alt = 'DEFAULT';
-                div.classList.add('products_square');
-                div.href = "product_detail.html";
-                box.appendChild(div);
+                p.innerHTML = `${data[i].bezeichnung}<br>${data[i].preis} €`;
+                divParent.appendChild(a);
+                a.appendChild(div);
                 div.appendChild(img);
                 div.appendChild(p);
-                i++;
             }
+            //var i = 0;
+            //for (let box of divParent.children) {
+            //    console.log("In der for loop");
+            //    console.log(data[i].preis);
+            //    console.log(data[i].bildpfad);
+            //    let img = document.createElement('img');
+            //    let p = document.createElement('p');
+            //    let div = document.createElement('div');
+            //    p.innerHTML = `${data[i].bezeichnung}<br>${data[i].preis} €`;
+            //    img.src = data[i].bildpfad;
+            //    img.alt = 'DEFAULT';
+            //    div.classList.add('products_square');
+            //    div.href = "product_detail.html";
+            //    box.appendChild(div);
+            //    div.appendChild(img);
+            //    div.appendChild(p);
+            //    i++;
+            //}
         } else {
             console.log('Status Unsuccessful look at this status Code ' + data.status);
             return data;
