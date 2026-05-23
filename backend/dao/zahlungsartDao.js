fabdf775333a88c82578c959c160820a98508c3d
Mehmet Akin <mehmet.a0504@gmail.com>
2026-05-23 22:13:51 +0200

    Endpoint für den Aufruf aller Zahlungsarten erstellt

--- backend/dao/zahlungsartDao.js
+++ backend/dao/zahlungsartDao.js
@@ -9,6 +9,17 @@ class ZahlungsartDao {
     getConnection() {
         return this._conn;
     }
+
+    loadAllPaymentMethods() {
+        var sql = 'SELECT * FROM Zahlungsart';
+        var statement = this._conn.prepare(sql);
+        var result = statement.all();
+
+        if (helper.isUndefined(result))
+            throw new Error('Keine Ressourceneinträge bei Zahlungsarten gefunden');
+
+        return result;
+    }
 }
 
 module.exports = ZahlungsartDao;
