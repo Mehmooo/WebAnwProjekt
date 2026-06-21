bffae27b25b2797a4bed3a2bdb3b1b8fd697ff43
Mehmoo <138767837+Mehmooo@users.noreply.github.com>
2026-06-21 20:42:37 +0200

    Adress Service und Dao eingefügt

--- backend/dao/adresseDao.js
+++ backend/dao/adresseDao.js
@@ -20,6 +20,78 @@ class AdresseDao {
 
         return result;
     }
+
+    loadAll() {
+        const landDao = new LandDao(this._conn);
+
+        var sql = 'SELECT * FROM Adresse';
+        var statement = this._conn.prepare(sql);
+        var result = statement.all();
+
+        if (helper.isArrayEmpty(result)) 
+            return [];
+
+        for (var i = 0; i < result.length; i++) {
+            result[i].land = landDao.loadById(result[i].landId);
+            delete result[i].landId;            
+        }
+
+        return result;
+    }
+
+    exists(id) {
+        var sql = 'SELECT COUNT(id) AS cnt FROM Adresse WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var result = statement.get(id);
+
+        if (result.cnt == 1) 
+            return true;
+
+        return false;
+    }
+
+    create(strasse = '', hausnummer = '', plz = '', ort = '') {
+        var sql = 'INSERT INTO Adresse (strasse,hausnummer,plz,ort) VALUES (?,?,?,?,?,?)';
+        var statement = this._conn.prepare(sql);
+        var params = [strasse, hausnummer, plz, ort];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not insert new Record. Data: ' + params);
+
+        return this.loadById(result.lastInsertRowid);
+    }
+
+    update(id, strasse = '', hausnummer = '', plz = '', ort = '') {
+        var sql = 'UPDATE Adresse SET strasse=?,hausnummer=?,adresszusatz=?,plz=?,ort=?,landId=? WHERE id=?';
+        var statement = this._conn.prepare(sql);
+        var params = [strasse, hausnummer, plz, ort, id];
+        var result = statement.run(params);
+
+        if (result.changes != 1) 
+            throw new Error('Could not update existing Record. Data: ' + params);
+
+        return this.loadById(id);
+    }
+
+    delete(id) {
+        try {
+            var sql = 'DELETE FROM Adresse WHERE id=?';
+            var statement = this._conn.prepare(sql);
+            var result = statement.run(id);
+
+            if (result.changes != 1) 
+                throw new Error('Could not delete Record by id=' + id);
+
+            return true;
+        } catch (ex) {
+            throw new Error('Could not delete Record by id=' + id + '. Reason: ' + ex.message);
+        }
+    }
+
+    toString() {
+        console.log('AdresseDao [_conn=' + this._conn + ']');
+    }
 }
 
 module.exports = AdresseDao;
